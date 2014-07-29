852d6a2a966bb10836113a241fd7adc6483d5286
Pau Maraví <paumb85@gmail.com>
2014-07-29 20:45:57 +0200

    Create blink.cpp
    A simple program for using a raspberry pi with arduino shield by cooking hack.
    The program is the blink's tutorial with some little modification

---
+++ blink.cpp
@@ -0,0 +1,36 @@
+#include "arduPi.h"
+
+/*********************************************************
+ *  IF YOUR ARDUINO CODE HAS OTHER FUNCTIONS APART FROM  *
+ *  setup() AND loop() YOU MUST DECLARE THEM HERE        *
+ * *******************************************************/
+
+/**************************
+ * YOUR ARDUINO CODE HERE *
+ * ************************/
+
+int led = 9;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  fprintf(stdout,"ON\n");
+  delay(5000);               // wait for 5 seconds
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  fprintf(stdout,"OFF\n");
+  delay(5000);               // wait for 5 seconds
+}
+
+int main (){
+        setup();
+        while(1){
+                loop();
+        }
+        return (0);
+}
